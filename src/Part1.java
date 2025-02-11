cdc6757f6ec1e85397b57e0d20c86b5372767a7d
AbdalrhmanAbu-Saif <abdalrhmanabusaif2002@gmail.com>
2025-02-11 16:27:02 +0300

    first commit

---
+++ src/Part1.java
@@ -0,0 +1,621 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+
+public class Part1 {
+    public static void main(String[] args) {
+
+        //Printing
+
+        //Part 1
+        //System.out.println("Ada Lovelace");
+
+
+        //Part2
+
+        /*
+        System.out.println("Once  Upon a time");
+        System.out.println("there was");
+        System.out.println("a  program");*/
+
+
+        // Reading  Input
+        /*
+        System.out.println("Write a message: ");
+        Scanner scanner =new Scanner(System.in);
+
+        String message = scanner.nextLine();
+
+        System.out.println(message);*/
+
+
+        //Part2
+
+      /*  System.out.println("Write a message: ");
+        Scanner scanner1 =new Scanner(System.in);
+
+        String name = scanner1.nextLine();
+
+        System.out.println("Hi "+name);*/
+
+        // Part 3
+
+      /*  System.out.println("Write a message: ");
+        Scanner scanner2 =new Scanner(System.in);
+
+        String msg = scanner2.nextLine();
+
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);*/
+
+
+        // Part 4
+
+      /*  System.out.println("Write  Something: ");
+        Scanner s1= new Scanner(System.in);
+        String s = s1.nextLine();
+
+        System.out.println("You  wrote "+s);*/
+
+        //part 5
+
+        /*System.out.println("What's your name?");
+        Scanner s3= new Scanner(System.in);
+        String s = s3.nextLine();
+
+        System.out.println("Hi "+s);*/
+
+
+        // part6
+
+
+       /* System.out.println("Greetings! How are you doing?");
+        Scanner a1=new Scanner(System.in);
+        String str1=a1.nextLine();
+
+        System.out.println("Oh, how interesting. Tell me more!");
+        Scanner a2=new Scanner(System.in);
+        String str2=a2.nextLine();
+
+        System.out.println("Thanks for sharing!");
+        Scanner a3=new Scanner(System.in);
+        String str3=a3.nextLine();
+
+
+        System.out.println("You  wrote: ");
+        System.out.println(str1+"\n"+str2+"\n"+str3);
+
+
+*/
+        //part 7
+
+       /* System.out.println("I will tell you a story, but I need some information first.\n" +
+                "What is the main character called?");
+        Scanner a5=new Scanner(System.in);
+        String str5=a5.nextLine();
+
+        System.out.println("What is their job?");
+        Scanner a6=new Scanner(System.in);
+        String str6=a6.nextLine();
+
+        System.out.println("Here is the story:\n" +
+                "Once upon a time there was "+str5+", who was a "+ str6+".\n" +
+                "On the way to work,"+str5+" reflected on life.\n" +
+                "Perhaps "+str5+" will not be a "+str6+" forever.");
+*/
+        //
+
+
+        // *********************************************************************************************************
+
+        // variable
+        // *********************************************************************************************************
+
+        //PAart1
+       /*
+        System.out.println("Chicken: ");
+        Scanner chicken=new Scanner(System.in);
+        int numchicken =chicken.nextInt();
+
+
+        System.out.println("Bacon (kg):");
+        Scanner Bacon=new Scanner(System.in);
+        float numbacon =Bacon.nextFloat();
+
+        System.out.println("Tractor:");
+        Scanner Tractor=new Scanner(System.in);
+        String tractor =Tractor.nextLine();
+
+        System.out.println("And finally, a summary:");
+        System.out.println(numchicken+"\n"+numbacon+"\n"+tractor);
+*/
+
+
+        // Part2
+ /*
+
+        System.out.println("Give a number:");
+
+        Scanner num1= new Scanner(System.in);
+
+        int  number1= num1.nextInt();
+        System.out.println("You gave the number "+number1);
+*/
+
+        //Part 3
+
+      /*  System.out.println("Give a number:");
+
+        Scanner num2= new Scanner(System.in);
+
+        float  number2= num2.nextFloat();
+        System.out.println("You gave the number "+number2);
+*/
+
+        //part 4
+        /*
+        System.out.println("Write something: ");
+
+        Scanner something1= new Scanner(System.in);
+        String  some1= something1.nextLine();
+
+        System.out.print("True or false?");
+        Scanner something2= new Scanner(System.in);
+        boolean  some2= something2.nextBoolean();
+*/
+
+        // part 5
+        /*
+        System.out.println("Give a string:");
+        Scanner str= new Scanner(System.in);
+        String  string= str.nextLine();
+
+        System.out.println("Give an integer : ");
+        Scanner num= new Scanner(System.in);
+        int Num= num.nextInt();
+
+        System.out.println("Give a double:");
+        Scanner doub= new Scanner(System.in);
+        double Doub= doub.nextDouble();
+
+
+        System.out.println("Give a boolean:");
+        Scanner bool= new Scanner(System.in);
+        boolean Bool= bool.nextBoolean();
+
+        System.out.println("You gave the string "+string+"\n"+
+                    "You gave the integer "+Num+"\n"+
+                "You gave the double "+Doub+"\n"+
+                "You gave the boolean "+Bool
+
+
+        );
+*/
+
+        // *********************************************************************************************************
+
+        // Calculating with numbers
+        // *********************************************************************************************************
+
+
+        //part1
+
+      /*  System.out.println("How many days would you like to convert to seconds?");
+
+        Scanner s1= new Scanner(System.in);
+        int S1=s1.nextInt();
+
+        int  calc= S1*24*60*60;
+        System.out.println(calc);
+    */
+
+
+        //Part2
+/*
+        System.out.println("Give the first number:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        System.out.println("Give the second number:");
+        Scanner s2= new Scanner(System.in);
+        int n2=s2.nextInt();
+
+        System.out.println("The sum of the numbers is "+(n1+n2));
+
+*/
+
+
+        ///part 3 ;
+
+   /*     System.out.println("Give the first number:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        System.out.println("Give the second number:");
+        Scanner s2= new Scanner(System.in);
+        int n2=s2.nextInt();
+
+        System.out.println("Give the third number:");
+        Scanner s3= new Scanner(System.in);
+
+
+
+
+
+
+
+        int n3=s3.nextInt();
+        System.out.println("The sum of the numbers is "+(n1+n2+n3));
+*/
+
+        //part4
+        /*
+        System.out.println("Give the first number:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+
+
+
+
+
+
+
+        System.out.println("Give the second number:");
+        Scanner s2= new Scanner(System.in);
+        int n2=s2.nextInt();
+
+        System.out.println(n1+" + "+n2 +" = "+(n1+n2));*/
+
+
+        //part 5
+
+        /*
+        System.out.println("Give the first number:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        System.out.println("Give the second number:");
+        Scanner s2= new Scanner(System.in);
+        int n2=s2.nextInt();
+
+        System.out.println(n1+" * "+n2 +" = "+(n1*n2)); */
+
+        //part 6
+
+/*
+        System.out.println("Give the first number:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        System.out.println("Give the second number:");
+        Scanner s2= new Scanner(System.in);
+        int n2=s2.nextInt();
+
+        double avg= (n1+n2)/2.0;
+        System.out.println("The average is  "+avg); */
+
+        //part 7
+
+/*
+        System.out.println("Give the first number:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        System.out.println("Give the second number:");
+        Scanner s2= new Scanner(System.in);
+        int n2=s2.nextInt();
+
+        System.out.println("Give the third number:");
+        Scanner s3= new Scanner(System.in);
+        int n3=s3.nextInt();
+        double avg= (n1+n2+n3)/3.0;
+        System.out.println("The average is  "+avg);
+*/
+
+
+        ///     //part 8
+
+    /*
+        System.out.println("Give the first number:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        System.out.println("Give the second number:");
+        Scanner s2= new Scanner(System.in);
+        int n2=s2.nextInt();
+
+        System.out.println(n1+" + "+n2 +" = "+(n1+n2)System.out.println("Give the first number:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        System.out.println("Give the second number:");
+        Scanner s2= new Scanner(System.in);
+        int n2=s2.nextInt(););
+        System.out.println(n1+" - "+n2 +" = "+(n1-n2));
+        System.out.println(n1+" * "+n2 +" = "+(n1*n2));
+        System.out.println(n1+" / "+n2 +" = "+(n1/(n2*1.0)));  */
+
+
+        // *********************************************************************************************************
+
+        // Conditional statements and conditional operation
+        // *********************************************************************************************************
+
+        //Part 1
+
+    /*    System.out.println("Give speed:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        if(n1>120){
+            System.out.println("Speeding ticket!\n");
+        }*/
+
+
+        // part 2
+/*
+        System.out.println("Give a number:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        if(n1==1984) {
+            System.out.println("Orwell");
+        }
+
+*/
+        // Part  3
+    /*
+        System.out.println("Give the first number:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        System.out.println("Give the second number:");
+        Scanner s2= new Scanner(System.in);
+        int n2=s2.nextInt();
+    /*    System.out.println("Give a year:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        if(n1<2015) {
+            System.out.println("Ancient history!");
+        }
+
+*/
+
+
+        // part 4
+
+ /*       System.out.println("Give a number :");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        if(n1>0) { // *********************************************************************************************************
+
+        // Conditional statements and conditional operation
+        // *********************************************************************************************************
+            System.out.println("The number is positive.");
+        }
+        else
+            System.out.println("The number is not   positive.");  */
+
+
+        // Part 5
+
+       /*
+        System.out.print("How old are you?\n");
+
+ // *********************************************************************************************************
+
+        // Conditional statements and conditional operation
+        // *********************************************************************************************************
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        if(n1>18) {
+            System.out.print("You are  an adult\n");
+        }
+        else
+            System.out.print("You are not an adult\n");   */
+
+
+        // Part 6
+
+        /*
+        System.out.println("Give the first number:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        System.out.println("Give the second number:");
+        Scanner s2= new Scanner(System.in);
+        int n2=s2.nextInt();
+
+        if(n1 > n2 )
+            System.out.println("Greater number is: "+n1);
+
+        else
+            System.out.println("Greater number is: "+n2);
+
+*/
+
+
+        // Part 7
+
+     /*
+     System.out.println("Give points [0-100]:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+         if(n1 <0)
+             System.out.println("Grade: impossible!");
+
+         else if (n1 >= 0 && n1 <= 49)
+             System.out.println("Grade:failed");
+
+         else if (n1 >= 50 && n1 <= 59)
+             System.out.println("Grade : 1");
+         else if (n1 >= 60 && n1 <= 69)
+             System.out.println("Grade : 2");
+
+         else if (n1 >= 70 && n1 <= 7System.out.println("Give points [0-100]:");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();9)
+             System.out.println("Grade : 3");
+
+         else if (n1 >= 80 && n1 <= 89)
+             System.out.println("Grade : 4");
+
+         else if (n1 >= 90 && n1 <= 100)
+             System.out.println("Grade : 5");
+
+         else if (n1 > 100)
+             System.out.println("Grade: incredible");
+
+
+*/
+
+        // part8
+
+      /*  System.out.println("Give a number:\n");
+
+        Scanner s1= new Scanner(System.in);
+        int n1=s1.nextInt();
+
+        if(n1 %2==0)
+            System.out.println("Number "+n1 +"is even");
+
+        else
+            System.out.println("Number "+n1 +" is odd");   */
+
+        // part 9
+     /*
+        System.out.println("Password?");
+        String  pass = "Caput Draconis";
+        Scanner s1= new Scanner(System.in);
+        String n1=s1.nextLine();
+
+         if(n1.equals(pass))
+             System.out.println("Welcome!");
+         else
+             System.out.println("Off with you!");
+*/
+        // Part 10
+
+  /*      System.out.println("Enter the first string:");
+
+        Scanner s1= new Scanner(System.in);
+        String n1=s1.nextLine();
+
+        System.out.println("Enter the second string:");
+
+        Scanner s2= new Scanner(System.in);
+        String n2=s2.nextLine();
+
+        if(n1.equals(n2))
+            System.out.println("Same");
+        else
+            System.out.println("Different");   */
+
+
+        // part 11
+/*
+        System.out.println("How  old are  you ? ");
+
+        Scanner s1= new Scanner(System.in);
+
+        int  n1=s1.nextInt();
+
+        if(n1>0 && n1<=120)
+            System.out.println("OK");
+        else
+
+            System.out.println("Impossible!");
+    */
+
+        // part 12
+/*
+        System.out.println("Give a year: ");
+
+        Scanner s1= new Scanner(System.in);
+
+        int  n1=s1.nextInt();
+
+        if((n1%4==0 && n1%100!=0 )||n1%400==0)
+            System.out.println("The year is a leap year.");
+        else
+            System.out.println("The year is not  a leap year");
+
+
+
+    }
+
+
+    */
+
+        // part 13
+/*
+        System.out.println("Value of the gift? ");
+        Scanner s1= new Scanner(System.in);
+
+        int  n1=s1.nextInt();
+
+        double tax=0;
+        if (n1 >=5000 && n1 <25000){
+             tax =(100 +(n1-5000)*0.08);
+            System.out.println("Tax: "+tax);
+
+
+        }
+
+        else if (n1 >=25000 && n1 <55000){
+            tax =(1700 +(n1-25000)*0.10);
+            System.out.println("Tax: "+tax);
+
+        }
+
+        else if (n1 >=55000 && n1 <200000){
+            tax =(4700 +(n1-55000)*0.12);
+            System.out.println("Tax: "+tax);
+
+        }
+
+        else if (n1 >=200000 && n1 <1000000){
+            tax =(22100 +(n1-200000)*0.15);
+            System.out.println("Tax: "+tax);
+
+        }
+
+        else if (n1 >=1000000){
+            tax =(142100 +(n1-25000)*0.10);
+            System.out.println("Tax: "+tax);
+
+        }
+        else
+            System.out.println("No tax!");
+
+*/
+        // *********************************************************************************************************
+
+        // Conditional statements and conditional operation
+        // *********************************************************************************************************
+
+
+    }
+}
