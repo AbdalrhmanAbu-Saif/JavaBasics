1e11818002f51e63d29e179ff7eb766f7b1b949d
AbdalrhmanAbu-Saif <abdalrhmanabusaif2002@gmail.com>
2025-02-17 17:30:15 +0300

    java rgramming|| part 8 with some erorr  i will  fix  it

--- src/JavaProgramming1/Part5/Objectsandreferences/DatingApp/SimpleDate.java
+++ src/JavaProgramming1/Part5/Objectsandreferences/DatingApp/SimpleDate.java
@@ -1,6 +1,8 @@
 package JavaProgramming1.Part5.Objectsandreferences.DatingApp;
 
-public class SimpleDate {   private int day;
+public class SimpleDate {
+
+    private int day;
     private int month;
     private int year;
 
